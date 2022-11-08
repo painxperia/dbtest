6c5b08f1380188e5bba062babf9f99a22fdb2558
zpzhang <zpzhang@cisco.com>
2022-11-08 14:06:17 +0800

    mybatis 流式操作

--- src/main/java/com/zpain/dbtest/service/UserService.java
+++ src/main/java/com/zpain/dbtest/service/UserService.java
@@ -2,6 +2,7 @@ package com.zpain.dbtest.service;
 
 import com.zpain.dbtest.entity.User;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.cursor.Cursor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +16,9 @@ public interface UserService {
     List<User> getAllUser(@Param("username") String username, @Param("password") String password);
 
     List<User> getUser(@Param("pageNum") Integer pageNum,@Param("pageSize") Integer pageSize);
+
+
+    Cursor<User> getUserCursor(@Param("limit") Integer limit);
+
+    int saveUser(List<User> list);
 }
--- src/test/java/com/zpain/dbtest/DbtestApplicationTests.java
+++ src/test/java/com/zpain/dbtest/DbtestApplicationTests.java
@@ -6,11 +6,17 @@ import com.github.pagehelper.PageInfo;
 import com.zpain.dbtest.entity.User;
 import com.zpain.dbtest.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.cursor.Cursor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StopWatch;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @SpringBootTest
 @Slf4j
@@ -30,6 +36,37 @@ class DbtestApplicationTests {
         List<User> user = userService.getUser(1, 2);
         PageInfo<User> userPageInfo = new PageInfo<>(user);
         log.info("list:{}", JSON.toJSONString(userPageInfo));
+    }
+
+    @Test
+    public void setUser(){
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        try {
+            List<User> list = IntStream.rangeClosed(1, 10000).mapToObj((i) -> {
+                User user = new User();
+                user.setId((long) i);
+                String format = String.format("zpain:%s", i);
+                user.setUsername(format);
+                user.setPassword(format);
+                user.setEmail(format);
+                return user;
+            }).collect(Collectors.toList());
+            userService.saveUser(list);
+            stopWatch.stop();
+            log.info("用时:{}",stopWatch.getTotalTimeSeconds());
+        }catch (Exception e){
+            log.error("e:",e);
+        }
+    }
+
+    @Test
+    @Transactional
+    public void getUser(){
+        Cursor<User>  list = userService.getUserCursor(20);
+        list.forEach( l->{
+            log.info("user:{}",JSON.toJSONString(l));
+        } );
     }
 
 }
